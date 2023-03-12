5eee5ffd8120ee509d0c2af434e5417ea13e12c2
Михаил Негрий <nemialtred@yandex.ru>
2023-03-12 12:39:23 +0200

    add: brain games gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,39 @@
+import startGame from '../index.js';
+import getRandomNumber from './getRandomNumber.js';
+
+const GAME_DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (firstNumber, secondNumber) => {
+  let biggestNumber;
+  let smallestNumber;
+
+  if (firstNumber < secondNumber) {
+    biggestNumber = secondNumber;
+    smallestNumber = firstNumber;
+  } else {
+    biggestNumber = firstNumber;
+    smallestNumber = secondNumber;
+  }
+
+  let result;
+
+  for (let i = 1; i <= biggestNumber / 2; i += 1) {
+    const canDivideBiggestNumber = biggestNumber % i === 0;
+    const canDivideSmallestNumber = smallestNumber % i === 0;
+    if (canDivideBiggestNumber && canDivideSmallestNumber) {
+      result = i;
+    }
+  }
+
+  return result;
+};
+
+export default () => {
+  startGame(GAME_DESCRIPTION, () => {
+    const firstNumber = getRandomNumber();
+    const secondNumber = getRandomNumber();
+    const answer = getGcd(firstNumber, secondNumber);
+
+    return [`${firstNumber} ${secondNumber}`, answer];
+  });
+};
