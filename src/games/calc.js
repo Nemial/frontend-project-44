e8a9ad0aa312131b07f746b45e9281ac06d4d11b
Михаил Негрий <nemialtred@yandex.ru>
2023-03-12 12:01:03 +0200

    add: brain calc game

---
+++ src/games/calc.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+import startGame, { getRandomNumber } from '../index.js';
+
+const GAME_DESCRIPTION = 'What is the result of the expression?';
+
+const getExpressionData = () => {
+  const operationSymbols = ['+', '-', '*'];
+  const firstIndex = 0;
+  const lastIndex = operationSymbols.length - 1;
+  const operationIndex = getRandomNumber(firstIndex, lastIndex);
+  const operationSymbol = operationSymbols[operationIndex];
+
+  const minNumber = 0;
+  const maxNumber = 450;
+  const firstOperand = getRandomNumber(minNumber, maxNumber);
+  const secondOperand = getRandomNumber(minNumber, maxNumber);
+
+  let expressionResult = 0;
+
+  switch (operationSymbol) {
+    case '+':
+      expressionResult = firstOperand + secondOperand;
+      break;
+    case '-':
+      expressionResult = firstOperand - secondOperand;
+      break;
+    case '*':
+      expressionResult = firstOperand * secondOperand;
+      break;
+    default:
+      console.log(`Undefined operation symbol ${operationSymbol}`);
+  }
+
+  return [`${firstOperand} ${operationSymbol} ${secondOperand}`, expressionResult];
+};
+
+export default () => {
+  startGame(GAME_DESCRIPTION, getExpressionData, readlineSync.questionInt);
+};
